565e7400b1ad4f81e2e06b7053ced52ab731036a
LAPTOP-6KDQGIEB\w4200 <420050757@qq.com>
2020-01-02 14:55:02 +0800

    add reverse proxy

--- main.go
+++ main.go
@@ -1,52 +1,43 @@
 package main
 
 import (
-	"os"
-	"hpor/rpc"
 	"flag"
 	log "github.com/sirupsen/logrus"
+	"hpor/rpc"
+	"os"
 )
 
-
-
-
-var t = flag.String("t", "RPC", "RPC or PROXY")
+var t = flag.String("t", "RP", "RPC or PROXY  or RP")
 var amqp = flag.String("url", "amqp://guest:guest@127.0.0.1:5672/", "amqp address")
 var port = flag.Int("p", 8000, "HTTP proxy port")
 var count = flag.Int("c", 1, "The number of consumers，work with -t RPC")
 var debug = flag.Bool("d", false, "log debug info")
 var queueName = flag.String("q", "rpc_queue", "The queue name for rpc")
 
-
 func main() {
 
 	flag.Parse()
 
 	log.SetOutput(os.Stdout)
-	if(*debug){
+	if *debug {
 		log.SetLevel(log.DebugLevel)
-	}else{
+	} else {
 		log.SetLevel(log.InfoLevel)
 	}
-	
-
-
-
 
 	conf := &rpc.Config{
-		Amqp:*amqp,
-		Port:*port,
-		QueueName:*queueName,
+		Amqp:      *amqp,
+		Port:      *port,
+		QueueName: *queueName,
 	}
 
+	log.Info("start app as ", *t)
 
-	log.Info("start app as ",*t)
-	
-	if(*t=="RPC"){
-		rpc.Accept(conf,*count)
-	}else{
+	if *t == "RPC" {
+		rpc.Accept(conf, *count)
+	} else if *t == "RP" {
+		rpc.ReverseProxy(conf)
+	} else {
 		rpc.Proxy(conf)
 	}
-
-
 }
--- rpc/server.go
+++ rpc/server.go
@@ -1,17 +1,22 @@
 package rpc
 
 import (
-	"io"
-	"fmt"
-	"github.com/streadway/amqp"
-	"net"
 	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
+
 	log "github.com/sirupsen/logrus"
+	"github.com/streadway/amqp"
 )
 
-
-func Proxy(conf *Config){
-	l, err := net.Listen("tcp",  fmt.Sprintf(":%d", conf.Port))
+func Proxy(conf *Config) {
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", conf.Port))
 	if err != nil {
 		log.Panic(err)
 	}
@@ -25,49 +30,91 @@ func Proxy(conf *Config){
 			log.Panic(err)
 		}
 		// client.SetReadDeadline(time.Now().Add(5 * time.Second))
-		go handleClientRequest(client,conn,conf)
+		go handleClientRequest(client, conn, conf)
+	}
+
+}
+
+var proxy_map = make(map[string]*httputil.ReverseProxy) //
+func ReverseProxy(conf *Config) {
+	vars := os.Environ()
+	for _, element := range vars {
+		// element is the element from someSlice for where we are
+		if strings.HasPrefix(element, "location") {
+			element = strings.Replace(element, "location", "", -1)
+			element = strings.Replace(element, "_", "/", -1)
+			a := strings.Split(element, "=")
+			log.Debug("location=", a[0], ",proxy_pass=", a[1])
+			remote, err := url.Parse(a[1])
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+
+			proxy := httputil.NewSingleHostReverseProxy(remote)
+			proxy_map[a[0]] = proxy
+			http.HandleFunc("/", handler())
+		}
+	}
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", conf.Port), nil)
+	if err != nil {
+		log.Error(err)
 	}
 }
 
-func handleClientRequest(client net.Conn,conn *amqp.Connection,conf *Config) {
+func handler() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Host, r.URL)
+		// w.Header().Set("X-Ben", "Rad")
+		for k, v := range proxy_map {
+			if strings.HasPrefix(r.URL.String(), k) {
+				v.ServeHTTP(w, r)
+				break
+			}
+		}
+		http.NotFound(w, r)
+	}
+}
+
+func handleClientRequest(client net.Conn, conn *amqp.Connection, conf *Config) {
 	if client == nil {
 		return
 	}
 	// defer client.Close()
 
-	rio,err := Dial(conn , conf)
-	failOnError(err,"cannot connect to mq")
+	rio, err := Dial(conn, conf)
+	failOnError(err, "cannot connect to mq")
 
 	rio.conn = client
 	defer rio.Close()
 
-
 	wait := make(chan bool)
 	//进行转发
-	go func(){
+	go func() {
 		io.Copy(rio, client)
 		log.Debug("1完成")
-		wait<-true
+		wait <- true
 	}()
-	go func(){
+	go func() {
 		io.Copy(client, rio)
 		log.Debug("2完成")
-		wait<-true
+		wait <- true
 	}()
-	
+
 	<-wait
 	log.Debug("关闭。。。")
 
 }
 
-func Dial(conn *amqp.Connection,conf *Config) (*rabbitIO, error) {
+func Dial(conn *amqp.Connection, conf *Config) (*rabbitIO, error) {
 
 	// defer conn.Close()
 
 	ch, err := conn.Channel()
-	if(err != nil){
-		log.Error("Failed to open a channel",err)
-		return nil,err
+	if err != nil {
+		log.Error("Failed to open a channel", err)
+		return nil, err
 	}
 	// failOnError(err, "Failed to open a channel")
 	// defer ch.Close()
@@ -75,7 +122,7 @@ func Dial(conn *amqp.Connection,conf *Config) (*rabbitIO, error) {
 	q, err := ch.QueueDeclare(
 		"",    // name
 		false, // durable
-		true, // delete when unused
+		true,  // delete when unused
 		true,  // exclusive
 		false, // noWait
 		nil,   // arguments
@@ -96,41 +143,39 @@ func Dial(conn *amqp.Connection,conf *Config) (*rabbitIO, error) {
 	corrId := randomString(32)
 
 	io := &rabbitIO{
-		correlationId:corrId,
-		ch:ch,
-		q:q,
-		msgs: msgs,
-		sender: true,
-		queueName:conf.QueueName,
+		correlationId: corrId,
+		ch:            ch,
+		q:             q,
+		msgs:          msgs,
+		sender:        true,
+		queueName:     conf.QueueName,
 	}
-	log.Info("new connection",corrId)
-	return io,nil
+	log.Info("new connection", corrId)
+	return io, nil
 }
 
-
-
-func Accept(conf *Config, count int){
+func Accept(conf *Config, count int) {
 	conn, err := amqp.Dial(conf.Amqp)
 	failOnError(err, "Failed to open a amqp connection")
-	for i:=0;i<count;i++ {
-		go accept(conf,conn)
+	for i := 0; i < count; i++ {
+		go accept(conf, conn)
 	}
 	forever := make(chan bool)
 	<-forever
 }
 
 var conn_map = make(map[string]*rabbitIO) //连接复用
-func accept(conf *Config,conn *amqp.Connection) {
+func accept(conf *Config, conn *amqp.Connection) {
 	ch, err := conn.Channel()
 	defer ch.Close()
 	failOnError(err, "Failed to open a channel")
 	q, err := ch.QueueDeclare(
 		conf.QueueName, // name
-		false,       // durable
-		false,       // delete when unused
-		false,       // exclusive
-		false,       // no-wait
-		nil,         // arguments
+		false,          // durable
+		false,          // delete when unused
+		false,          // exclusive
+		false,          // no-wait
+		nil,            // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
@@ -151,36 +196,36 @@ func accept(conf *Config,conn *amqp.Connection) {
 		nil,    // args
 	)
 	failOnError(err, "Failed to register a consumer")
-    
+
 	log.Printf(" [*] Awaiting RPC requests")
 	for d := range msgs {
-		log.Debug("read ",len(d.Body)," bytes")
+		log.Debug("read ", len(d.Body), " bytes")
 
-		c,ok := conn_map[d.CorrelationId]
+		c, ok := conn_map[d.CorrelationId]
 		// log.Debug(d.CorrelationId,ok)
-		if(!ok){
+		if !ok {
 			c := &rabbitIO{
-				correlationId:d.CorrelationId,
-				ch:ch,
-				q:q,
-				msgs: msgs,
-				sender:false,
-				replyTo:d.ReplyTo,
-				queueName:conf.QueueName,
-				buffer:bytes.NewBuffer([]byte{}),
+				correlationId: d.CorrelationId,
+				ch:            ch,
+				q:             q,
+				msgs:          msgs,
+				sender:        false,
+				replyTo:       d.ReplyTo,
+				queueName:     conf.QueueName,
+				buffer:        bytes.NewBuffer([]byte{}),
 			}
 			conn_map[d.CorrelationId] = c
 			c.replyTo = d.ReplyTo
 			c.correlationId = d.CorrelationId
-			
+
 			go c.CreateRequest(d.Body)
 			// copy(b[0:10], d.Body[0:10])
-		}else{
-			go func(){
-				if(c.conn != nil){
-					io.Copy(c.conn,c.buffer)
+		} else {
+			go func() {
+				if c.conn != nil {
+					io.Copy(c.conn, c.buffer)
 					c.conn.Write(d.Body)
-				}else{
+				} else {
 					c.buffer.Write(d.Body)
 				}
 			}()
@@ -190,4 +235,3 @@ func accept(conf *Config,conn *amqp.Connection) {
 	forever := make(chan bool)
 	<-forever
 }
-
